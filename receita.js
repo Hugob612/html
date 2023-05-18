efa3c6d3614f4c8277b51652002a8e1cc0e10b13
Hugob612 <133262080+Hugob612@users.noreply.github.com>
2023-05-18 17:38:32 -0300

    Add files via upload

---
+++ receita.js
@@ -0,0 +1,13 @@
+const nomeReceita = "Bolo de cenoura"
+const ingredientes = [
+    "2 cenouras medias",
+    "3 ovos",
+    "3~4 de xicara de oleo",
+    "1 chicara de acucar",
+    "2 xicaras de trigo",
+    "1 colher de cha de fermento em po"
+]
+
+ingredientes.forEach(ingrediente => {
+    console.log(ingrediente)    
+});
